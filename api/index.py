d1b703b749caca0ace0fece74810c83485835911
gwozai <63190594+gwozai@users.noreply.github.com>
2024-04-18 15:45:53 +0800

    机器人启动

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from api import app
-
-
+from api.messagechannel.qywx.qywxbot import WeChatBot
+bot = WeChatBot('4e35a96d-134b-45fa-9c5a-f3d4f65670f6')
+bot.send_text('pushed 启动！')
 if __name__ == '__main__':
     app.run()

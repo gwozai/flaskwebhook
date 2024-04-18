787da15509ea63747bd18a832bdb30e72b9a819a
gwozai <63190594+gwozai@users.noreply.github.com>
2024-04-18 16:12:08 +0800

    1

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from api import app
 from api.messagechannel.qywx.qywxbot import WeChatBot
+# 没有走main方法，获取app直接就运行了
+try:
+    bot = WeChatBot('4e35a96d-134b-45fa-9c5a-f3d4f65670f6')
+    bot.send_text('消息通知 启动！')
+except Exception as e:
+    pass
 
 if __name__ == '__main__':
-    try:
-        bot = WeChatBot('4e35a96d-134b-45fa-9c5a-f3d4f65670f6')
-        bot.send_text('pushed 启动！')
-    except Exception as e:
-        pass
+
     app.run()
